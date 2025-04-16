11fc4662d06d7c61f6058b14ab98573a8ea21517
Alejandro Zepeda <alexzepeda@Alejandros-MacBook-Pro-5.local>
2025-04-16 15:03:45 +0200

    testing_subhamiltonian file

---
+++ examples/test_subsections.py
@@ -0,0 +1,28 @@
+from mctdh_input.builders.hamiltonian_builder import HamiltonianBuilder, HamiltonianSubsection
+from mctdh_input.utils.electronic_ops import ElectronicOperatorFactory
+
+# Initialize builder
+dofs = ["el", "R"] + [f"x{i}" for i in range(1, 49)]
+builder = HamiltonianBuilder(degrees_of_freedom_labels=dofs)
+
+# Create operator factory for 3 electronic states
+factory = ElectronicOperatorFactory(num_states=6)
+
+# Create a purely electronic section
+electronic_section = HamiltonianSubsection("Purely Electronic Terms")
+electronic_section.add_term("1.0", [("el", factory.Snm(1, 2))])
+electronic_section.add_term("0.5", [("el", factory.Znm(3, 3))])
+electronic_section.add_term("0.1", [("el", factory.identity())])
+
+# Add to builder
+builder.add_subsection(electronic_section)
+
+# Create a nuclear-electronic coupling section (just for fun)
+coupling_section = HamiltonianSubsection("Coupling Terms")
+coupling_section.add_term("0.25", [("el", factory.Snm(2, 1)), ("R", "x")])  # R is a nuclear mode
+
+builder.add_subsection(coupling_section)
+
+# Write to file
+builder.generate_file("examples/hamiltonian_test.txt")
+print("Hamiltonian file written to examples/hamiltonian_test.txt")
